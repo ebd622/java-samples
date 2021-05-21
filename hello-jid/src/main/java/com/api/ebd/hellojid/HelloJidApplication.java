44379d5fc748f603ee6426b1932bfc5dc08675e0
Eugeny <eburdo@gmail.com>
2021-05-21 17:06:45 +0200

    initial commit

---
+++ hello-jid/src/main/java/com/api/ebd/hellojid/HelloJidApplication.java
@@ -0,0 +1,13 @@
+package com.api.ebd.hellojid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloJidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloJidApplication.class, args);
+	}
+
+}
---
+++ hello-jid/src/main/java/com/api/ebd/hellojid/model/Auto.java
@@ -0,0 +1,28 @@
+package com.api.ebd.hellojid.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import java.time.LocalDate;
+
+@Table(name = "person")
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Auto {
+    @Id
+    @GeneratedValue
+    private int id;
+    private String brand;
+    private String type;
+    private LocalDate birthDate;
+}
