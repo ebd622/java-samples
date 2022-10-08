69dc248d99025a4938fdc9af7fa256f857c01dfd
Eugeny <eburdo@gmail.com>
2022-10-08 10:48:16 +0200

    add examples

--- java17/samples/src/main/java/org/ebd/samples/jep406/TypePattern.java
+++ java17/samples/src/main/java/org/ebd/samples/jep406/TypePattern.java
@@ -1,5 +1,17 @@
 package org.ebd.samples.jep406;
 
 public class TypePattern {
-    //TODO
+    public static void main(String[] args) {
+        patternMach("5");
+        patternMach(5);
+    }
+
+    private static void patternMach(Object o) {
+        if (o instanceof String s) {
+            System.out.println(String.format("Object is a string %s", s));
+        } else if(o instanceof Number n) {
+            System.out.println(String.format("Object is a string %d", n));
+        }
+    }
+
 }
