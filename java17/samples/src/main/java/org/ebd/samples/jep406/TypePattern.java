0560b9c1dc7f71d983072690b4cf94b01a4b2708
Eugeny <eburdo@gmail.com>
2022-10-08 11:12:54 +0200

    add examples

--- java17/samples/src/main/java/org/ebd/samples/jep406/TypePattern.java
+++ java17/samples/src/main/java/org/ebd/samples/jep406/TypePattern.java
@@ -3,24 +3,34 @@ package org.ebd.samples.jep406;
 public class TypePattern {
     public static void main(String[] args) {
 
-        System.out.println("Int -> Double: " + getDoubleUsingIf(5));
-        System.out.println("Float -> Double: " + getDoubleUsingIf(5.0f));
-        System.out.println("String -> Double: " + getDoubleUsingIf("5"));
-
+        System.out.println(formatter(5));
+        System.out.println(formatter(5d));
+        System.out.println(formatter(5l));
+        System.out.println(formatter("5"));
     }
 
     // Option 1: using if-else
-    static double getDoubleUsingIf(Object o) {
-        double result;
-        if (o instanceof Integer) {
-            result = ((Integer) o).doubleValue();
-        } else if (o instanceof Float) {
-            result = ((Float) o).doubleValue();
-        } else if (o instanceof String) {
-            result = Double.parseDouble(((String) o));
-        } else {
-            result = 0d;
+    static String formatter(Object o) {
+        String formatted = "unknown";
+        if (o instanceof Integer i) {
+            formatted = String.format("int %d", i);
+        } else if (o instanceof Long l) {
+            formatted = String.format("long %d", l);
+        } else if (o instanceof Double d) {
+            formatted = String.format("double %f", d);
+        } else if (o instanceof String s) {
+            formatted = String.format("String %s", s);
         }
-        return result;
+        return formatted;
     }
+
+//    static String formatterPatternSwitch(Object o) {
+//        return switch (o) {
+//            case Integer i -> String.format("int %d", i);
+//            case Long l    -> String.format("long %d", l);
+//            case Double d  -> String.format("double %f", d);
+//            case String s  -> String.format("String %s", s);
+//            default        -> o.toString();
+//        };
+//    }
 }
