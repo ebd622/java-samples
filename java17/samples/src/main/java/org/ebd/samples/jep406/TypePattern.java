f3b9ddc484ecf821521ce59603d4191a0e77b9c6
Eugeny <eburdo@gmail.com>
2022-10-08 10:59:28 +0200

    add examples

--- java17/samples/src/main/java/org/ebd/samples/jep406/TypePattern.java
+++ java17/samples/src/main/java/org/ebd/samples/jep406/TypePattern.java
@@ -2,7 +2,11 @@ package org.ebd.samples.jep406;
 
 public class TypePattern {
     public static void main(String[] args) {
-        System.out.println(String.format("Int -> Double: %f", getDoubleUsingIf(5)));
+        //System.out.println(String.format("Int -> Double: %f", getDoubleUsingIf(5)));
+        //System.out.println(String.format("Int -> Double: %f", getDoubleUsingIf(5.0)));
+
+        System.out.println("Int -> Double: " + getDoubleUsingIf(5));
+        System.out.println("Float -> Double: " + getDoubleUsingIf(5.0f));
 
     }
 
