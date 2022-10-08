4d96bc8c19dbae0ddee8d592592e59185cd5fd02
Eugeny <eburdo@gmail.com>
2022-10-08 10:54:44 +0200

    add examples

--- java17/samples/src/main/java/org/ebd/samples/jep406/TypePattern.java
+++ java17/samples/src/main/java/org/ebd/samples/jep406/TypePattern.java
@@ -2,16 +2,22 @@ package org.ebd.samples.jep406;
 
 public class TypePattern {
     public static void main(String[] args) {
-        patternMach("5");
-        patternMach(5);
+        System.out.println(String.format("Int -> Double: %f", getDoubleUsingIf(5)));
+
     }
 
-    private static void patternMach(Object o) {
-        if (o instanceof String s) {
-            System.out.println(String.format("Object is a string %s", s));
-        } else if(o instanceof Number n) {
-            System.out.println(String.format("Object is a string %d", n));
+    // Option 1
+    static double getDoubleUsingIf(Object o) {
+        double result;
+        if (o instanceof Integer) {
+            result = ((Integer) o).doubleValue();
+        } else if (o instanceof Float) {
+            result = ((Float) o).doubleValue();
+        } else if (o instanceof String) {
+            result = Double.parseDouble(((String) o));
+        } else {
+            result = 0d;
         }
+        return result;
     }
-
 }
