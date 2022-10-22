5ae23b5c70c8656c74b8b2a2645dd8f602d005bc
Eugeny <eburdo@gmail.com>
2022-10-22 11:17:43 +0200

    add examples

--- java17/samples/src/main/java/org/ebd/samples/jep406/PatternRefining.java
+++ java17/samples/src/main/java/org/ebd/samples/jep406/PatternRefining.java
@@ -2,10 +2,23 @@ package org.ebd.samples.jep406;
 
 public class PatternRefining {
     public static void main(String[] args) {
+        Shape s = new Shape();
+        testTriangle(s);
 
     }
 
-    class Shape {}
-    class Rectangle extends Shape {}
-    class Triangle  extends Shape { int calculateArea() { return 110; } }
+
+    static void testTriangle(Shape s) {
+        switch (s) {
+            case Triangle t && (t.calculateArea() > 100) ->
+                    System.out.println("Large triangle");
+            case Triangle t ->
+                    System.out.println("Small triangle");
+            default ->
+                    System.out.println("Non-triangle");
+        }
+    }
+    static class Shape {}
+    static class Rectangle extends Shape {}
+    static class Triangle  extends Shape { int calculateArea() { return 110; } }
 }
