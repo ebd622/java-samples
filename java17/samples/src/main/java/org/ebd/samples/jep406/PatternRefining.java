b9375fd08e097a976bdb15e5a69fe7b1a32f9969
Eugeny <eburdo@gmail.com>
2022-10-22 11:07:48 +0200

    add examples

--- java17/samples/src/main/java/org/ebd/samples/jep406/PatternRefining.java
+++ java17/samples/src/main/java/org/ebd/samples/jep406/PatternRefining.java
@@ -2,7 +2,10 @@ package org.ebd.samples.jep406;
 
 public class PatternRefining {
     public static void main(String[] args) {
-        
+
     }
 
+    class Shape {}
+    class Rectangle extends Shape {}
+    class Triangle  extends Shape { int calculateArea() { return 110; } }
 }
