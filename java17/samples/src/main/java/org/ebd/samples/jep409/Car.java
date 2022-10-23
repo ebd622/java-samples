a680c89cf47e3e99b4066ae17ea91b2d5477f4b6
Eugeny <eburdo@gmail.com>
2022-10-23 11:54:46 +0200

    add examples

--- java17/samples/src/main/java/org/ebd/samples/jep409/Car.java
+++ java17/samples/src/main/java/org/ebd/samples/jep409/Car.java
@@ -1,4 +1,21 @@
 package org.ebd.samples.jep409;
 
-public class Car {
+public non-sealed class Car extends Vehicle implements Service {
+
+    private final int numberOfSeats;
+
+    public Car(int numberOfSeats, String registrationNumber) {
+        super(registrationNumber);
+        this.numberOfSeats = numberOfSeats;
+    }
+
+    public int getNumberOfSeats() {
+        return numberOfSeats;
+    }
+
+    @Override
+    public int getMaxServiceIntervalInMonths() {
+        return 12;
+    }
+
 }
