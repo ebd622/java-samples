2e3eeb4ca17b9cd63254b913d36498fa1b59c3a1
Eugeny <eburdo@gmail.com>
2022-10-23 12:16:02 +0200

    add examples

--- java17/samples/src/main/java/org/ebd/samples/jep409/Example.java
+++ java17/samples/src/main/java/org/ebd/samples/jep409/Example.java
@@ -3,6 +3,10 @@ package org.ebd.samples.jep409;
 public class Example {
     public static void main( String[] args ){
 
-        System.out.println( "Hello World!" );
+        // Instanciate a sealed class
+        Service truck = new Truck(5000, "G-1000-PN");
+
+        
+        System.out.println( truck.getMaxServiceIntervalInMonths() );
     }
 }
