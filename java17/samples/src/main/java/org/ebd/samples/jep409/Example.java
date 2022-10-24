244374aacf53b353a24435c2ba96888e707d1200
Eugeny <eburdo@gmail.com>
2022-10-24 13:29:59 +0200

    add examples

--- java17/samples/src/main/java/org/ebd/samples/jep409/Example.java
+++ java17/samples/src/main/java/org/ebd/samples/jep409/Example.java
@@ -7,11 +7,11 @@ public class Example {
 
         // Instanciate a sealed class
         Service truck = new Truck(5000, "G-1000-PN");
-        System.out.println( truck.getMaxServiceIntervalInMonths() );
+        System.out.println( "Track, MaxServiceIntervalInMonths: " + truck.getMaxServiceIntervalInMonths() );
 
         // Instanciate a non-sealed class (which estends a sealed class and implements a sealed interface)
         Service car = new Car(1500, "3-TRT-88");
-        System.out.println( car.getMaxServiceIntervalInMonths() );
+        System.out.println( "Car, MaxServiceIntervalInMonths: " + car.getMaxServiceIntervalInMonths() );
     }
 
     /**
