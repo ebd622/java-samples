7d2c4a5f46bdd3c064cca55f1ac86227ba69c59c
Eugeny <eburdo@gmail.com>
2022-10-26 17:54:45 +0200

    add examples

--- java17/samples/src/main/java/org/ebd/samples/jep409/Example.java
+++ java17/samples/src/main/java/org/ebd/samples/jep409/Example.java
@@ -13,19 +13,21 @@ public class Example {
         // Instanciate a non-sealed class (which estends a sealed class and implements a sealed interface)
         Service car = new Car(1500, "3-TRT-88");
         System.out.println( "Car, MaxServiceIntervalInMonths: " + car.getMaxServiceIntervalInMonths() );
+
+        Service companyCar = new CompanyCar(1300, "G-319-PB");
     }
 
-    /**
-     * CompanyCar can extend non-sealed {@link Car} but not {@link Track}
-     */
-    class CompanyCar extends Car {
-        private final String merk = "Nissan";
-        
-        public CompanyCar(int numberOfSeats, String registrationNumber){
-            super(numberOfSeats,registrationNumber);
-        }
-        public String getMerk(){
-            return merk;
-        }
+}
+/**
+ * CompanyCar can extend non-sealed {@link Car} but not {@link Track}
+ */
+class CompanyCar extends Car {
+    private final String merk = "Nissan";
+
+    public CompanyCar(int numberOfSeats, String registrationNumber){
+        super(numberOfSeats,registrationNumber);
+    }
+    public String getMerk(){
+        return merk;
     }
 }
