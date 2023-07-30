f15bf7f833e2a61bbfdb48b5ec6a9ee1d90f8b07
Eugeny <eburdo@gmail.com>
2023-07-30 19:11:02 +0200

    change logic

--- httpclient/src/main/java/com/ebd/demo/httpclient/SSLMutualAuthTest.java
+++ httpclient/src/main/java/com/ebd/demo/httpclient/SSLMutualAuthTest.java
@@ -1,6 +1,7 @@
 package com.ebd.demo.httpclient;
 
 import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
@@ -22,12 +23,15 @@ public class SSLMutualAuthTest {
     public static void main(String[] args) throws Exception {
         if(args.length < 3){
             System.out.println("No args provided");
-            System.out.println("Usage: java -jar SSLMutualAuthTest <truststore.jks> <password> <alias> <https://example.com/path/api> ");
+            System.out.println("Usage: java -jar httpclient-0.0.1.jar <truststore.jks> <password> <alias> <https://example.com/path/api> <GET|POST> ");
             System.exit(0);
         }
+        System.out.println("---> TrustStore: " + args[0]);
+        System.out.println("---> pass: " + args[1]);
+        System.out.println("---> alias: " + args[2]);
+        System.out.println("---> URL: " + args[3]);
+        System.out.println("---> HTTP Method: " + args[4]);
 
-        System.out.println("---> Run SSLMutualAuthTest");
-        String CERT_ALIAS = "onboarding-organisations";
         KeyStore identityKeyStore = KeyStore.getInstance("jks");
         FileInputStream identityKeyStoreFile = new FileInputStream(args[0]);
 
@@ -38,16 +42,13 @@ public class SSLMutualAuthTest {
         trustKeyStore.load(trustKeyStoreFile, args[1].toCharArray());
 
         SSLContext sslContext = SSLContexts.custom()
-
                 // load identity keystore
                 .loadKeyMaterial(identityKeyStore, args[1].toCharArray(), new PrivateKeyStrategy() {
-
                     @Override
                     public String chooseAlias(Map<String, PrivateKeyDetails> aliases, Socket socket) {
-                        return CERT_ALIAS;
+                        return args[2];
                     }
                 })
-
                 // load trust keystore
                 .loadTrustMaterial(trustKeyStore, null)
                 .build();
@@ -62,9 +63,19 @@ public class SSLMutualAuthTest {
                 .build();
 
         // Call a SSL-endpoint
-        HttpUriRequest httpPost = new HttpPost("https://distributie.services.rabobank.nl/klant/GetRelation/7/");
-
-        HttpResponse httpresponse = client.execute(httpPost);
+        HttpUriRequest httpRequest;
+        switch (args[4]) {
+            case "GET":
+                httpRequest = new HttpGet(args[3]);
+                break;
+            case "POST":
+                httpRequest = new HttpPost(args[3]);
+                break;
+            default:
+                httpRequest = new HttpPost(args[3]);
+                break;
+        }
+        HttpResponse httpresponse = client.execute(httpRequest);
 
         //printing the status line
         System.out.println(httpresponse.getStatusLine());
