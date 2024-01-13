1a8c51cb1204e8e9566a9fce71fa2e0af0bf2efa
Eugeny <eburdo@gmail.com>
2024-01-13 23:17:36 +0100

    add example

--- java21/samples21/src/main/java/org/ebd/samples21/jep444/Sample1.java
+++ java21/samples21/src/main/java/org/ebd/samples21/jep444/Sample1.java
@@ -3,11 +3,15 @@ package org.ebd.samples21.jep444;
 public class Sample1 {
     public static void doWork() {
         try {
-            Thread.sleep(5000);
+            //code...
+            //VT is unmounted from PT
+            Thread.sleep(3000);
+            //VT is mounted to PT again
+            //code...
         } catch (InterruptedException e) {}
     }
     public static void main(String[] args) {
-        int MAX=10000;
+        int MAX=10000000;
         for(var i=0; i<MAX; i++) {
             //new Thread(Sample1::doWork).start();
             Thread.startVirtualThread(Sample1::doWork);
