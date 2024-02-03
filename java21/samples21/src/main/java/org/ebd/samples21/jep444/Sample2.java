f6eec3df8426b6f05ffcad1616bb8a3237f3a42f
Eugeny <eburdo@gmail.com>
2024-02-03 22:14:50 +0100

    play with VT

--- java21/samples21/src/main/java/org/ebd/samples21/jep444/Sample2.java
+++ java21/samples21/src/main/java/org/ebd/samples21/jep444/Sample2.java
@@ -2,7 +2,12 @@ package org.ebd.samples21.jep444;
 
 public class Sample2 {
     public static void fetch(int index, String path){
-        
+        try{
+            //TODO
+
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+        }
     }
     public static void main(String...arg){
 
