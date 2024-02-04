ec3a057a4f7f61ad85a597b0164d12659f8e8480
Eugeny <eburdo@gmail.com>
2024-02-04 21:46:49 +0100

    play with VT

--- java21/samples21/src/main/java/org/ebd/samples21/jep444/Sample2.java
+++ java21/samples21/src/main/java/org/ebd/samples21/jep444/Sample2.java
@@ -1,10 +1,13 @@
 package org.ebd.samples21.jep444;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class Sample2 {
     public static void fetch(int index, String path){
         try{
             System.out.println(index + " before " + Thread.currentThread());
-            
+            var numberOfLines = Files.lines(Paths.get(path)).count();
             //TODO
 
         } catch (Exception e){
