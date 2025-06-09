65de6a8e49142a7a3fafbe4676da62afc9ab7196
Eugeny <eburdo@gmail.com>
2025-06-09 22:09:02 +0200

    clean up

--- dop-app/src/main/java/client/SampleTrade.java
+++ dop-app/src/main/java/client/SampleTrade.java
@@ -1,4 +1,21 @@
 package client;
 
+import trade.Buy;
+import trade.Sell;
+import trade.Trade;
+
 public class SampleTrade {
+    public static void main(String[] args) {
+        processTrade(new Buy("Product1", 200));
+        processTrade(new Buy("Product2", 30000));
+
+        processTrade(new Sell("Product1", 300));
+        processTrade(new Sell("Product2", 50000));
+
+    }
+
+    private static void processTrade(Trade trade) {
+        AuditRules.audit(trade);
+        trade.perform();
+    }
 }
