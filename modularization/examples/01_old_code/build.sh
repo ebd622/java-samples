62b67131297b235014369a2234726f6161f82ce7
ebd622 <eburdo@gmail.com>
2025-12-28 22:14:53 +0100

    add examples

--- modularization/examples/01_old_code/build.sh
+++ modularization/examples/01_old_code/build.sh
@@ -1,16 +1,21 @@
+
 /bin/rm -rf output
 
+# Create output directories
 mkdir -p output/mlib
 mkdir -p output/classes
 
+# Compile and package the first module (first.jar)
 javac -d output/classes -classpath output/mlib `find firstAPI -name *.java`
 jar -c -f output/mlib/first.jar -C output/classes .
 
 /bin/rm -rf ouput/classes
 mkdir -p output/classes
 
+# Compile and package the client module (client.jar)
 javac -d output/classes -classpath output/mlib/first.jar `find client -name *.java`
 jar -c -f output/mlib/client.jar -C output/classes .
 
+# Run the client application
 java -classpath output/mlib/first.jar:output/mlib/client.jar com.myorg.client.ClientApp
 
--- modularization/examples/01_old_code/client/com/myorg/client/ClientApp.java
+++ modularization/examples/01_old_code/client/com/myorg/client/ClientApp.java
@@ -6,10 +6,7 @@ class ClientApp {
   public String getInfo() { return "This is ClientApp"; }
 
   public static void main(String args[]) {
-    var second = new ClientApp();
-
-    System.out.println(second.getInfo());
-
+    System.out.println(new ClientApp().getInfo());
     System.out.println(new FirstApi().getInfo());
   }
 }
