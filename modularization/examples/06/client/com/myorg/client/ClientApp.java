226d61a3b1018fe5f87a0326216fd1eb68d20593
ebd622 <eburdo@gmail.com>
2026-01-02 22:17:07 +0100

    add examples

--- modularization/examples/06/client/com/myorg/client/ClientApp.java
+++ modularization/examples/06/client/com/myorg/client/ClientApp.java
@@ -1,13 +1,27 @@
 package com.myorg.client;
 
 import com.myorg.first.FirstApi;
-//import module com.myorg.theFirstAPI;
+import com.myorg.first.Helper;
+import java.lang.reflect.Method;
+
+//import com.myorg.impl.HelperImpl; //Error: Cannot access com.myorg.impl.HelperImpl
 
 class ClientApp {
   public String getInfo() { return "This is ClientApp, it is a part of " + ClientApp.class.getModule(); }
 
-  public static void main(String args[]) {
+  public static void main(String args[]) throws Exception {
     System.out.println(new ClientApp().getInfo());
     System.out.println(new FirstApi().getInfo());
+
+    //--- Accessing Helper class and its methods
+    Helper helper = new FirstApi().getHelper();
+    System.out.println(helper);
+    System.out.println(helper.greet()); //works fine
+
+    // --- Accessing secret() method via reflection
+    Method method = helper.getClass().getMethod("secret");
+    System.out.println(method);
+    System.out.println(method.invoke(helper)); //Error:  Java says "I'm watching you"
+
   }
 }
