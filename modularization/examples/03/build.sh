dc38b908f23a441123936e5f8e21cc32bd855e77
ebd622 <eburdo@gmail.com>
2026-02-26 21:00:46 +0100

    update readme

--- modularization/examples/03/build.sh
+++ modularization/examples/03/build.sh
@@ -16,13 +16,13 @@ mkdir -p output/classes
 javac -d output/classes -classpath output/mlib/first.jar `find client -name \*.java`
 jar -c -f output/mlib/client.jar -C output/classes .
 
-echo "--- All code running from the classpath belong to the one [unnamed module] ---"
+echo "--- Code running from the classpath belong to the one [unnamed module] ---"
 
 # Run the client application
 java -classpath output/mlib/first.jar:output/mlib/client.jar com.myorg.client.ClientApp
 
 echo ""
-echo "--- All old code running from the modulepath belong to their own automatic module ---"
+echo "--- The same code running from the modulepath belong to their own automatic module ---"
 ## -p output/mlib: Sets the module path to the output/mlib directory, where modular JARs are located.
 ## -m client/com.myorg.client.ClientApp: Runs the main class com.myorg.client.ClientApp from the client module.
 java -p output/mlib -m client/com.myorg.client.ClientApp
