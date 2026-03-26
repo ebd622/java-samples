3138aec9ad8c6b115b1b6cbf2d2fa21b83f8a9b3
ebd622 <eburdo@gmail.com>
2026-03-26 23:05:32 +0100

    update readme

--- modularization/examples/06/clientAPI/com/myorg/client/ClientApi.java
+++ modularization/examples/06/clientAPI/com/myorg/client/ClientApi.java
@@ -2,7 +2,7 @@ package com.myorg.client;
 
 public class ClientApi {
   public String getInfo() {
-    return "This is ClientApi, it is a part of " + ClientApi.class.getModule() ;
+    return "This is ClientApi, it is a part of: " + ClientApi.class.getModule() ;
   }
   public com.myorg.client.Helper getHelper() {
     return new com.myorg.impl.HelperImpl();
